8135760d6be8655863e1dc4015ebb3d8a54d0a08
Bedenashvili David <bedenashvili@mindbox-external.cloud>
2024-12-19 09:54:28 +0300

    created new project

---
+++ film-scraper/scraper.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	c := colly.NewCollector
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+}
